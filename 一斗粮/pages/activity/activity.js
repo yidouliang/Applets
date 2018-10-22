9add7b2f91df75cf68d80870aab91ec74179fe87
Anntly <btzhang@foxmail.com>
2018-10-22 16:59:38 +0800

    增加底部导航栏

---
+++ 一斗粮/pages/activity/activity.js
@@ -0,0 +1,56 @@
+Page({
+  data: {
+    list3: [
+      {
+        icon: 'https://gw.alipayobjects.com/zos/rmsportal/VBqNBOiGYkCjqocXjdUj.png',
+        text: '标题文字',
+        desc: '描述信息',
+      },
+      {
+        icon: 'https://gw.alipayobjects.com/zos/rmsportal/VBqNBOiGYkCjqocXjdUj.png',
+        text: '标题文字',
+        desc: '描述信息',
+      },
+      {
+        icon: 'https://gw.alipayobjects.com/zos/rmsportal/VBqNBOiGYkCjqocXjdUj.png',
+        text: '标题文字',
+        desc: '描述信息',
+      },
+      {
+        icon: 'https://gw.alipayobjects.com/zos/rmsportal/VBqNBOiGYkCjqocXjdUj.png',
+        text: '标题文字',
+        desc: '描述信息',
+      },
+      {
+        icon: 'https://gw.alipayobjects.com/zos/rmsportal/VBqNBOiGYkCjqocXjdUj.png',
+        text: '标题文字',
+        desc: '描述信息',
+      },
+      {
+        icon: 'https://gw.alipayobjects.com/zos/rmsportal/VBqNBOiGYkCjqocXjdUj.png',
+        text: '标题文字',
+        desc: '描述信息',
+      },
+      {
+        icon: 'https://gw.alipayobjects.com/zos/rmsportal/VBqNBOiGYkCjqocXjdUj.png',
+        text: '标题文字',
+        desc: '描述信息',
+      },
+      {
+        icon: 'https://gw.alipayobjects.com/zos/rmsportal/VBqNBOiGYkCjqocXjdUj.png',
+        text: '标题文字',
+        desc: '描述信息',
+      },
+      {
+        icon: 'https://gw.alipayobjects.com/zos/rmsportal/VBqNBOiGYkCjqocXjdUj.png',
+        text: '标题文字',
+        desc: '描述信息',
+      },
+    ],
+  },
+  onItemClick(ev) {
+    my.alert({
+      content: ev.detail.index,
+    });
+  },
+});
