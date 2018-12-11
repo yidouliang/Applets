8a536ca64f66b1bc56b6adfd4a16ee4d05f6e1e4
Anntly <btzhang@foxmail.com>
2018-12-11 16:51:02 +0800

    修改订单展示

--- 一斗粮/pages/myOrder/myOrder.js
+++ 一斗粮/pages/myOrder/myOrder.js
@@ -1,5 +1,31 @@
 Page({
-  data: {},
+   data: {
+    tabs: [
+      {
+        title: '待付款',
+        //badgeType: 'text',
+        //badgeText: '6',
+      },
+      {
+        title: '已付款',
+      }
+    ],
+    activeTab: 0,
+    paidOrder: [], //已支付订单
+    waitOrder: [], //未支付订单
+  },
+  handleTabClick({ index }) {
+    this.setData({
+      activeTab: index,
+      
+    });
+  },
+  handleTabChange({ index }) {
+    this.setData({
+      activeTab: index,
+      
+    });
+  },
 
   onLoad() {
     this.getOrderInfo();
@@ -13,13 +39,23 @@ Page({
           my.httpRequest({
           url:'http://localhost:9090/api/order/' + consumerId,
           success: (res) => {
-          this.setData({
-            orderNum: res.data.data[0]['serialNumber'],
-          orderStatus: res.data.data[0]['orderStatus'],
-          createTime: res.data.data[0]['createTime'],
-          payAmount: res.data.data[0]['payAmount'],
-          orderItems: res.data.data[0]['orderInfoDetailDTOS']
-          });
+            var that = this
+            console.log(this.activeTab)
+            if(this.data.activeTab == 0){
+              if(res.data.data[0]['orderStatus'] ==='待支付'){
+
+                    that.setData({
+                      paidOrder:res.data.data[0],
+                    });
+              }
+            }else{
+              if(res.data.data[0]['orderStatus'] === '已支付'){
+                    that.setData({
+                      waitOrder:res.data.data[0],
+                    });
+              }
+            }
+          
         
           console.log(res.data.data[0]['orderStatus']);
         },
